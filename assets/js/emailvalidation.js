32a3f3ff52b9e1b083e1b8b5dda83c12a823091f
Thomas James <wjamesthomas3@gmail.com>
2024-07-30 21:20:57 -0400

    Split JS into separate files

---
+++ assets/js/emailvalidation.js
@@ -0,0 +1,63 @@
+/* Email Validator Functions */
+/* AlphaNumeric Algorithm => https://stackoverflow.com/questions/4434076/best-way-to-alphanumeric-check-in-javascript */
+/* Credit ^^ Michael Martin-Smucker */
+/* Acceptable Email Formats => https://help.xmatters.com/ondemand/trial/valid_email_format.htm */
+/* Email guidelines => https://help.xmatters.com/ondemand/trial/valid_email_format.htm */
+
+/* Check For Alpha Numeric Characters */
+function isAlphaNum(charCode) {
+    if (!(charCode >= 48 && charCode <= 57) &&  // 0-9
+        !(charCode >= 65 && charCode <= 90) &&  // A-Z
+        !(charCode >= 97 && charCode <= 122)) { // a-z
+            return false;
+    }
+    return true;
+}
+
+/* Check For Allowed Non-Alpha Numeric Characters For Emails */
+/* code 95 (underscore), code 45 (dash), code 46 (period) */
+function isExceptionChar(charCode) {
+    return (charCode === 95 || charCode === 45 || charCode === 46);
+}
+
+function validateCharacters(partition) {
+    for (let i = 0; i < partition.length; i++) {
+        const charCode = partition.charCodeAt(i);
+        /* If Character Is NOT Alpha AND Is Not ('.', '-', OR '_'), Invalidate */
+        if (!isAlphaNum(charCode) && !isExceptionChar(charCode)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isValidEmailConvention(elementVal) {
+    /* Split email input into two parts -- `john-smith@examp.com` ==> john-smith, examp.com */
+    const inputChars = elementVal.split('@');
+    const emailPrefix = inputChars[0];
+    const emailDomain = inputChars[1];
+
+    /* Grab last character for check (MUST BE ALPHA) */
+    const lastCharOfPrefix = emailPrefix.charCodeAt(emailPrefix.length - 1);
+    
+    /* If Email Is Not Parsed Successfully Into Prefix & Domain, Invalidate */
+    if (inputChars.length !== 2) {
+        return false;
+    }
+
+    /* If The Last Character Of Email Prefix Is Not Alpha, Invalidate */
+    if (!isAlphaNum(lastCharOfPrefix)) {
+        return false;
+    }
+
+    /* Iterate Through Partitions For Validating Individual Characters */
+    const isValidPrefix = validateCharacters(emailPrefix);
+    if (!isValidPrefix) { return false; }
+    const isValidDomain = validateCharacters(emailDomain);
+    if (!isValidDomain) { return false; }
+
+    /* ALL TESTS ARE PASSED, ALLOW VALIDATION */
+    return true;
+}
+
+export default isValidEmailConvention;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+import isValidEmailConvention from './emailvalidation.js';
+
 /* DEBUG: FNS FOR LOGGING */
 function logTargetValue(e) {
     console.log(e.target.value)
@@ -20,68 +22,6 @@ function debounce(fn, delay) {
         clearTimeout(debounceTimer);
         debounceTimer = setTimeout(() => { fn.apply(this, args) }, delay);
     }
-}
-
-/* Email Validator Functions */
-/* AlphaNumeric Algorithm => https://stackoverflow.com/questions/4434076/best-way-to-alphanumeric-check-in-javascript */
-/* Credit ^^ Michael Martin-Smucker */
-/* Acceptable Email Formats => https://help.xmatters.com/ondemand/trial/valid_email_format.htm */
-/* Email guidelines => https://help.xmatters.com/ondemand/trial/valid_email_format.htm */
-
-/* Check For Alpha Numeric Characters */
-function isAlphaNum(charCode) {
-    if (!(charCode >= 48 && charCode <= 57) &&  // 0-9
-        !(charCode >= 65 && charCode <= 90) &&  // A-Z
-        !(charCode >= 97 && charCode <= 122)) { // a-z
-            return false;
-    }
-    return true;
-}
-
-/* Check For Allowed Non-Alpha Numeric Characters For Emails */
-/* code 95 (underscore), code 45 (dash), code 46 (period) */
-function isExceptionChar(charCode) {
-    return (charCode === 95 || charCode === 45 || charCode === 46);
-}
-
-function validateCharacters(partition) {
-    for (let i = 0; i < partition.length; i++) {
-        const charCode = partition.charCodeAt(i);
-        /* If Character Is NOT Alpha AND Is Not ('.', '-', OR '_'), Invalidate */
-        if (!isAlphaNum(charCode) && !isExceptionChar(charCode)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function isValidEmailConvention(elementVal) {
-    /* Split email input into two parts -- `john-smith@examp.com` ==> john-smith, examp.com */
-    const inputChars = elementVal.split('@');
-    const emailPrefix = inputChars[0];
-    const emailDomain = inputChars[1];
-
-    /* Grab last character for check (MUST BE ALPHA) */
-    const lastCharOfPrefix = emailPrefix.charCodeAt(emailPrefix.length - 1);
-    
-    /* If Email Is Not Parsed Successfully Into Prefix & Domain, Invalidate */
-    if (inputChars.length !== 2) {
-        return false;
-    }
-
-    /* If The Last Character Of Email Prefix Is Not Alpha, Invalidate */
-    if (!isAlphaNum(lastCharOfPrefix)) {
-        return false;
-    }
-
-    /* Iterate Through Partitions For Validating Individual Characters */
-    isValidPrefix = validateCharacters(emailPrefix);
-    if (!isValidPrefix) { return false; }
-    isValidDomain = validateCharacters(emailDomain);
-    if (!isValidDomain) { return false; }
-
-    /* ALL TESTS ARE PASSED, ALLOW VALIDATION */
-    return true;
 }
 
 /* ========================== */
@@ -154,29 +94,29 @@ function handleForm(event) {
         3. Handle Error State
     */
     /* First Name */
-    firstNameRequired = document.getElementById('fn-required');
+    const firstNameRequired = document.getElementById('fn-required');
     handleTextRequired(firstNameInput, firstNameRequired);
 
     /* Last Name */
-    lastNameRequired = document.getElementById('ln-required');
+    const lastNameRequired = document.getElementById('ln-required');
     handleTextRequired(lastNameInput, lastNameRequired);
 
     /* Email */
-    emailRequired = document.getElementById('em-required');
-    emailInvalid = document.getElementById('em-invalid');
+    const emailRequired = document.getElementById('em-required');
+    const emailInvalid = document.getElementById('em-invalid');
     handleTextRequired(emailInput, emailRequired);
     handleValidEmail(emailInput, emailInvalid);
 
     /* Query Type */
-    queryRequired = document.getElementById('qt-required');
+    const queryRequired = document.getElementById('qt-required');
     handleQueryChecked(queryTypeInputs, queryRequired);
     
     /* Message */
-    messageRequired = document.getElementById('msg-required');
+    const messageRequired = document.getElementById('msg-required');
     handleTextRequired(messageInput, messageRequired);
 
     /* Terms */
-    termsRequired = document.getElementById('terms-required');
+    const termsRequired = document.getElementById('terms-required');
     handleTermsChecked(termsInput, termsRequired);
     
 }
